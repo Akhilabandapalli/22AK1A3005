9751cd8b23de84e61aa0f0098e21cfb0a0c20af7
Akhilabandapalli <akhilabandapalli@gmail.com>
2024-09-14 19:47:02 +0530

    adding my file

---
+++ relationaloperators.js
@@ -0,0 +1,11 @@
+// Create two large integers
+let num1 = BigInt("123456789012345678901234567890");
+let num2 = BigInt("987654321098765432109876543210");
+
+// Demonstrate relational operations
+console.log("num1 < num2:", num1 < num2);
+console.log("num1 > num2:", num1 > num2);
+console.log("num1 <= num2:", num1 <= num2);
+console.log("num1 >= num2:", num1 >= num2);
+console.log("num1 === num2:", num1 === num2);
+console.log("num1 !== num2:", num1 !== num2);
